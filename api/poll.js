eff574169fd11dc6a061291438a30eb6843c1e71
RamsesCode <RamsesSanchez25@gmail.com>
2025-07-22 17:37:13 -0400

    fixed an issue in the import method so it should run now

--- api/poll.js
+++ api/poll.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { authMiddleware } = require("../auth/middleware");
+const authMiddleware = require("../auth/middleware");
 const { Polls } = require("../database");
 
 
